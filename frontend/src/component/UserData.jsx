e09a064e25a7ca667746ac832c748b9a427df987
Mr.Susheel Kumar <74233895+Susheel4115@users.noreply.github.com>
2022-05-31 22:25:15 +0530

    Update UserData.jsx

--- frontend/src/component/UserData.jsx
+++ frontend/src/component/UserData.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import SearchBAr from './SearchBAr'
-import SideBar from './common/SideBar'
-import Header from './common/Header'
-import DataTable from './DataTable'
-import './userData.css'
+import React from "react";
+import SearchBAr from "./SearchBAr";
+import SideBar from "./common/SideBar";
+import Header from "./common/Header";
+import DataTable from "./DataTable";
+import "./CSS-property/userData.css";
 const UserData = () => {
   return (
-    <div className='userData-container'>
-    <SideBar/>
-    <div className='right-side' >
-      <Header/>
-      <div className='rectangle'></div>
-      <SearchBAr/>
-      <DataTable/>
-
+    <div className="userData-container">
+      <SideBar />
+      <div className="right-side">
+        <Header />
+        <div className="rectangle"></div>
+        <SearchBAr />
+        <DataTable />
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default UserData
+export default UserData;
